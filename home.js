de5ed5ad77fbad435f76f14b0a6d2150a3b73465
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-15 13:01:25 +0200

    update on hackthon project group3

---
+++ home.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".words", {
+        strings: ["MADE UP", "SCHOOL"],
+        typeSpeed: 50,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
